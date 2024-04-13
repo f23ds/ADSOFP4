ca59a6045929c456b9cd79814c7a9c44a9389c21
f23ds <fabiodesio23@gmail.com>
2024-04-13 19:15:43 +0200

    "Binary files of various classes in the components, exceptions, interfaces, notifications, test, and blockchain/utils packages have been modified."

---
+++ src/Components/SimpleValidate.java
@@ -0,0 +1,12 @@
+package Components;
+
+import Interfaces.IMiningMethod;
+import Interfaces.IValidateMethod;
+
+public class SimpleValidate implements IValidateMethod {
+
+  @Override
+  public boolean validate(IMiningMethod miningMethod, Block block) {
+    return miningMethod.createHash(block) == block.getHash();
+  }
+}
---
+++ src/Interfaces/IValidateMethod.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+import Components.Block;
+
+public interface IValidateMethod {
+  public boolean validate(IMiningMethod miningMethod, Block block);
+}
